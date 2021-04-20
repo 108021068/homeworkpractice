7716b6e29c543cde10b54d667ab21995515608d7
108021068 <56907745+108021068@users.noreply.github.com>
2021-04-20 20:44:11 +0800

    Update testwk.py

--- testwk.py
+++ testwk.py
@@ -1,39 +1,51 @@
+
 import hashlib
-import hmac
 import sys
-import binascii
+md5fortext = []
+sha1fortext = []
+sha256fortext = []
+sha512fortext = []
+text = []
+if len(sys.argv) == 2:
+    msg = sys.argv[1].encode('utf-8')
+    #sha256做的 hash 值
+    hashvaluetosha256 = hashlib.sha256(msg)
+    print(hashvaluetosha256)
+    print(hashvaluetosha256.hexdigest().upper())
+    print()
+    #md5做的 hash 值
+    hashvaluetomd5 = hashlib.md5(msg)
+    print(hashvaluetomd5)
+    print(hashvaluetomd5.hexdigest().upper())
+    print()
+    #sha1做的 hash 值
+    hashvaluetosha1 = hashlib.sha1(msg)
+    print(hashvaluetosha1)
+    print(hashvaluetosha1.hexdigest().upper())
+    print()
+    #sha512做的 hash 值
+    hashvaluetosha512 = hashlib.sha512(msg)
+    print(hashvaluetosha512)
+    print(hashvaluetosha512.hexdigest().upper())
+    print()
 
-passwordList = []
-with open ('passwordList.txt', 'r')as filehandle:
-    for line in filehandle:
-        currentPlace = line[:-1]
-        passwordList.append(currentPlace)
-    #print(passwordList)
-if len (sys.argv) ==2:
-    msg = sys.argv[1].encode()
-    hashValue = hashlib.sha1(msg)
+f = open('D://10-million-password-list-top-100.txt')
+for line in f:
+    text.append(line.split("\n")[0])
+    sha256fortext.append(hashlib.sha256(line.split("\n")[0].encode()).hexdigest().upper())
+    sha1fortext.append(hashlib.sha1(line.split("\n")[0].encode()).hexdigest().upper())
+    sha512fortext.append(hashlib.sha512(line.split("\n")[0].encode()).hexdigest().upper())
+    md5fortext.append(hashlib.md5(line.split("\n")[0].encode()).hexdigest().upper())
+    #print(hashfortext)
 
-if len(sys.argv)>1:
-    msg= sys.argv[1].encode()
-    hashvalue = hashlib.sha256(msg)
-    print(hashvalue)
-    print(hashvalue.hexdigest().upper())
-
-if len(sys.argv)>1:
-    msg= sys.argv[1].encode()
-    hashvalue = hashlib.sha512(msg)
-
-    print(hashvalue)
-    print(hashvalue.hexdigest().upper())
-
-if len(sys.argv)>1:
-    msg= sys.argv[1].encode()
-    hashvalue = hashlib.sha1(msg)
-    print(hashvalue)
-    print(hashvalue.hexdigest().upper())
-
-if len(sys.argv)>1:
-    msg= sys.argv[1].encode()
-    hashvalue = hashlib.md5(msg)
-    print(hashvalue)
-    print(hashvalue.hexdigest().upper())
+if len(sys.argv) > 2:
+    if sys.argv[1] == "-c":  
+        for i in range(len(text)):
+            if sys.argv[2] == sha1fortext[i]: 
+                print("<password is",text[i],"><sha1 Hash>")
+            elif sys.argv[2] == sha256fortext[i]: 
+                print("<password is",text[i],"><sha256 Hash>")
+            elif sys.argv[2] == sha512fortext[i]: 
+                print("<password is",text[i],"><sha512 Hash>")
+            elif sys.argv[2] == md5fortext[i]: 
+                print("<password is",text[i],"><md5 Hash>")
